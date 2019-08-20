044302e6152b5eeb9453b9a1f1a30d63e5a53fe3
Charlie Kelly <charliekelly31415@gmail.com>
2019-08-20 09:57:28 -0400

    add album selection (not working)

--- samples/photo_album/src/components/PhotoList.js
+++ samples/photo_album/src/components/PhotoList.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { openUploadWidget } from '../utils/CloudinaryService';
-import { photosUploaded } from '../actions';
+import { photosUploaded, albumSelected } from '../actions';
 import Photo from './Photo';
-import { ALBUM_TAG } from '../utils/Constants';
+import { Image, Transformation } from 'cloudinary-react';
+import albumList from '../config/albums';
 
 class PhotoList extends Component {
+    constructor () {
+        super();
+        this.state = {
+            selectedAlbum: null
+        }
+    }
+
+    handleAlbumSelect = event => {
+        const elem = event.target;
+        // clicked element either has to be div or Image
+        const album = elem.id || elem.parentNode.id;
+        const albumName = album.id;
+        console.log('Album selected: ', albumName);
+        this.setState({
+            selectedAlbum: albumName
+        });
+        this.props.onAlbumSelect(albumName);
+    }
+
     render() {
         return (
             <div className="photoList">
 
-                <div className="actions">
+                {/* <div className="actions">
                     <NavLink className="upload_link" exact to="/photos/new">
                         Add photo with React File upload
                     </NavLink>
-                </div>
+                </div> */}
 
-                <h1>Your Photos</h1>
-                <h3>(tag: "{ALBUM_TAG}")</h3>
+                <div className="album-list">
+                    <h2>Select Album</h2>
+                    {albumList.albums.map(album => {
+                        return (
+                            <div key={album.tag} id={album.tag} className="album-btn" onClick={this.handleAlbumSelect}>
+                                <Image  
+                                    publicId={`art/${album.thumbnail}`}
+                                    className="thumbnail inline"
+                                    width="150"
+                                    height="150"
+                                    crop="fit"
+                                    quality="80"
+                                >
+                                    <Transformation quality="auto" fetchFormat="auto" />
+                                </Image>
+                            </div>
+                        );
+                    })}
+                </div>
+                {this.state.selectedAlbum ? (
+                    <h1>Selected Album: 
+                        <span className="album-name">{this.state.selectedAlbum}</span>
+                    </h1>
+                ) : (
+                    <h2>Please select an album</h2>
+                )}
+                
                 
                 <div className="photos">
                     {this.props.photos.length === 0 && (
@@ -34,23 +79,12 @@ class PhotoList extends Component {
                         );
                     })}
                 </div>
-                <div className="note">
-                    Take a look at our documentation of{' '}
-                    <a
-                        href="https://cloudinary.com/documentation/image_transformations"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Image Transformations
-                    </a>{' '}
-                    for a full list of supported transformations.
-                </div>
             </div>
         );
     }
 
     uploadImageWithCloudinary() {
-        const uploadOptions = { tags: ALBUM_TAG, ...this.context };
+        const uploadOptions = { ...this.context };
         openUploadWidget(uploadOptions, (error, photos) => {
             if (!error) {
                 this.props.onPhotosUploaded(photos);
@@ -64,6 +98,7 @@ class PhotoList extends Component {
 PhotoList.propTypes = {
     photos: PropTypes.array,
     onPhotosUploaded: PropTypes.func,
+    onAlbumSelect: PropTypes.func,
 };
 
 PhotoList.contextTypes = {
@@ -75,6 +110,7 @@ const PhotoListContainer = connect(
     state => ({ photos: state.photos }),
     {
         onPhotosUploaded: photosUploaded,
+        onAlbumSelect: albumSelected
     }
 )(PhotoList);
 
